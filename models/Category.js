049b976e0b81658766c2b456d81eea094cb55072
jennyk1989 <jennifer0keene@gmail.com>
2022-02-01 20:41:20 -0600

    Category and Product models updated

--- models/Category.js
+++ models/Category.js
@@ -4,13 +4,25 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// Models can be defined by extending Model and calling init(attributes, options)
 Category.init(
+  // attributes
   {
     // define columns
-    // TODO: id column -> integer, NO null values, primary key, auto increment
-
+    // id column -> integer, NO null values, primary key, auto increment
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     // TODO: category_name column -> string, NO null values
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
+  // options
   {
     sequelize,
     timestamps: false,
--- models/Product.js
+++ models/Product.js
@@ -10,15 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-    // TODO: id column -> int, NO null values, primary key, auto increment
-
-    // TODO: product_name column -> string, NO null values
-
-    // TODO: price column -> decimal, no null values, validates value is decimal
-
-    // TODO: stock column -> int, no null values, default value of 10, validates value is numeric
-
-    // TODO: category_id column -> int, references the category model's id
+    // id column -> int, NO null values, primary key, auto increment
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // product_name column -> string, NO null values
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // price column -> decimal, no null values, validates value is decimal
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: { isDecimal: true },
+    },
+    // stock column -> int, no null values, default value of 10, validates value is numeric
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: { isNumeric: true },
+    },
+    // category_id column -> int, references the category model's id
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
