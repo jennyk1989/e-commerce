6c10979d1e15ff4afef417c0312e3de012906b5f
jennyk1989 <jennifer0keene@gmail.com>
2022-02-01 19:59:57 -0600

    added more packages and updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const routes = require('./routes');
-// TODO: import sequelize connection
+// import sequelize connection
+const sequelize = require('sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+// parse requests in JSON format
+app.use(express.json()); 
+// parse requests in url format
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // TODO: sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync().then(() => { 
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
