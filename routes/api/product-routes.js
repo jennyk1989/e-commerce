4034540b758c5195e631af52fc2e65e5395f87ee
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-04 18:58:43 -0600

    product routes updated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,53 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({ //Sequelize's findAll method
+    // be sure to include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        // Category model columns include id & category_name
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        // Tag model columns: id, tag_name
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(data => {
+    res.json (data);
+    if (err) res.status(500).send('products query not successful');
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    // ...by it's id
+    where: {
+      id: req.params.id
+    },
+    // be sure to include its associated Category and Tag data
+    include: [ 
+      {
+        model: Category,
+        // Category model columns include id & category_name
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        // Tag model columns: id, tag_name
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(data => {
+    res.json (data);
+    if (err) res.status(500).send('product query not successful');
+  });
 });
 
 // create new product
@@ -25,26 +65,30 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  Product.create(
+    //returns all the columns in Product model
+    req.body
+  )
+  .then((product) => {
+    // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+    if (req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+      return ProductTag.bulkCreate(productTagIdArr);
+    }
+    // if no product tags, just respond
+    res.status(200).json(product);
+  })
+  .then((productTagIds) => res.status(200).json(productTagIds))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+  
 });
 
 // update product
@@ -91,6 +135,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({ //Sequelize's destroy method
+    // single out a product by its id
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    res.json (data);
+    if (err) res.status(500).send('product query not successful');
+  });
 });
 
 module.exports = router;
